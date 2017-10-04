89b56c2fa6f16a8212d8591dd61e3d5f5c6c8653
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-04 12:48:01 -0500

    update

--- hobby.py
+++ hobby.py
@@ -68,29 +68,31 @@ class ThankyouHandler(TemplateHandler):
 class TipcalcHandler(TemplateHandler):
     def get(self):
         self.render_template('tipcalc.html', {})
+
     def post(self):
         bill = self.get_body_argument('bill')
         service = self.get_body_argument('service')
+        values = self.handleTip(bill, service)
+        self.render_template('tipcalc.html', {'values': values})
 
-    def main():
-        # ask for bill amount
-        bill = float(input("What is the amount of your bill? $"))
-
-        # ask for satisfaction level
-        service = input("\nHow was the service?\ngood\nfair\nbad\nPlease choose one of the above options, then press Enter. ").lower()
-
+    def handleTip(self, bill, service):
+        bill = float(bill)
+        print(bill, service)
 
         # calculate tip amount
         tips = {"good" : 0.20,
                 "fair" : 0.15,
-                "bad" : 0.10}
+                "poor" : 0.10}
         tip_percentage = tips[service]
+        print(tip_percentage)
+
         tip = bill * tip_percentage
+        total = tip + bill
 
-        # print message with tip amount
+        print(tip, service, total)
         print("\nYour bill was ${bill:.2f}, and your service was {service}.\n\nYou should tip ${tip:.2f}\n".format(bill=bill, service=service, tip=tip))
+        return (tip, service, total)
 
-        main()
 
 
 class ContactHandler(TemplateHandler):
